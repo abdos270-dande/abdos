746c2019e23a0df0d6063fd1f9f9114f9080375f
abdos270-dande <frotoabds2@gmail.com>
2025-07-05 13:37:05 +0300

    Update save_winner.php

--- save_winner.php
+++ save_winner.php
@@ -1,10 +1,9 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-  $data = json_decode(file_get_contents("php://input"), true);
-  if (isset($data["winner"])) {
-    file_put_contents("winner.txt", $data["winner"]);
-    echo json_encode(["status" => "saved"]);
-  } else {
-    echo json_encode(["status" => "no_winner"]);
-  }
-}
+$data = json_decode(file_get_contents("php://input"), true);
+$name = $data["winner"];
+
+$conn = new mysqli("sql304.infinityfree.com", "if0_39393071", "YjlT8G8HtGbMEC", "if0_39393071_draw_app");
+$conn->query("UPDATE winner SET name = '$name' WHERE id = 1");
+
+echo json_encode(["status" => "saved"]);
+?>
