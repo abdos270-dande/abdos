4d447a631dfc0cc092335914593e2070418aabf9
abdos270-dande <frotoabds2@gmail.com>
2025-07-05 14:06:46 +0300

    Update draw_config.php

--- draw_config.php
+++ draw_config.php
@@ -1,7 +1,10 @@
 <?php
-header('Content-Type: application/json');
 $conn = new mysqli("sql304.infinityfree.com", "if0_39393071", "YjlT8G8HtGbMEC", "if0_39393071_draw_app");
+$conn->set_charset("utf8");
+
 $result = $conn->query("SELECT start_time FROM draw_config WHERE id = 1");
 $row = $result->fetch_assoc();
+
+header("Content-Type: application/json");
 echo json_encode(["start_time" => date(DATE_ISO8601, strtotime($row["start_time"]))]);
 ?>
