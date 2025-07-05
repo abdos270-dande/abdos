9b2eeaaa0cc61028419e928a50a52ba252c06f07
abdos270-dande <frotoabds2@gmail.com>
2025-07-05 13:48:10 +0300

    Update get_winner.php

--- get_winner.php
+++ get_winner.php
@@ -1,6 +1,11 @@
 <?php
+header('Content-Type: application/json');
+
 $conn = new mysqli("sql304.infinityfree.com", "if0_39393071", "YjlT8G8HtGbMEC", "if0_39393071_draw_app");
+$conn->set_charset("utf8");
+
 $result = $conn->query("SELECT name FROM winner WHERE id = 1");
 $row = $result->fetch_assoc();
-echo $row ? $row["name"] : "";
+
+echo json_encode(["name" => $row ? $row["name"] : null]);
 ?>
