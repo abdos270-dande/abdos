dc156e7fd1d3ff4f6775b808f8c8030fdc840aec
abdos270-dande <frotoabds2@gmail.com>
2025-07-05 12:30:20 +0300

    Update draw_config.php

--- draw_config.php
+++ draw_config.php
@@ -1,4 +1,6 @@
 <?php
+// هذا الملف يتم جلب وقت بداية القرعة منه
+header("Content-Type: application/json");
 echo json_encode([
-  "draw_start" => "2025-07-05T12:40:00+03:00"
+  "start_time" => "2025-07-05T17:00:00+03:00"
 ]);
