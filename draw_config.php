9f8ba09e6e2198ca72a69a792472669843b19428
abdos270-dande <frotoabds2@gmail.com>
2025-07-05 13:02:24 +0300

    Update draw_config.php

--- draw_config.php
+++ draw_config.php
@@ -1,6 +1,7 @@
 <?php
 header("Content-Type: application/json");
+
+// التاريخ والوقت بصيغة ISO 8601 (غيره حسب رغبتك)
 echo json_encode([
-  "start_time" => "2025-07-05T12:55:00+03:00",  // يبدأ بعده عداد 5 دقائق
-  "winner" => file_exists("winner.txt") ? file_get_contents("winner.txt") : ""
+  "start_time" => "2025-07-05T17:00:00+03:00"
 ]);
