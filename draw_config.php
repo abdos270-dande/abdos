1adfaa7e7bdd8733b62d591a4ca67184e6c47eb5
abdos270-dande <frotoabds2@gmail.com>
2025-07-05 13:20:40 +0300

    Update draw_config.php

--- draw_config.php
+++ draw_config.php
@@ -1,7 +1,5 @@
 <?php
 header("Content-Type: application/json");
-
-// التاريخ والوقت بصيغة ISO 8601 (غيره حسب رغبتك)
 echo json_encode([
-  "start_time" => "2025-07-05T01:05:00+03:00"
+  "start_time" => "2025-07-05T01:25:00+03:00"
 ]);
